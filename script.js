4e935b192096086cd92cd44e04ed8840b6eab688
Moataz Ibrahim <motazeibrahim3@gmail.com>
2023-07-08 19:07:31 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,423 @@
+    // Question:1
+    // var num1=Number(window.prompt('Enter your Number'));
+    // document.getElementById('demo').innerHTML =num1 ;
+
+    //  Question:2
+    //  var userNumb=Number(window.prompt('Enter your Number'));
+    //  if(userNumb%3==0 && userNumb%4==0)
+    //  {
+    //     document.getElementById('demo').innerHTML ='Yes';
+    //  }
+    //  else
+    //  {
+    //     document.getElementById('demo').innerHTML ='No';
+    //  }
+        
+     
+    // Question:3
+    // var num1=Number(window.prompt('Enter your Number')) ;
+    // var num2=Number(window.prompt('Enter your Number')) ;
+    // if(num1 > num2)
+    // {
+    //     document.getElementById('demo').innerHTML =num1
+    // }
+    // else
+    // {
+    //     document.getElementById('demo').innerHTML =num2
+    // }
+    
+
+    //  Question:4
+    // var num1=Number(window.prompt('Enter your Number')) ;
+  
+    // if( num1 >0 )
+    // {
+    //     document.getElementById('demo').innerHTML ='Postive'
+    // }
+    // else
+    // {
+    //     document.getElementById('demo').innerHTML ='Negative'
+    // }
+    
+   //  Question:5
+   //  var num1=Number(window.prompt('Enter your Number')) ;
+   //  var num2=Number(window.prompt('Enter your Number')) ;
+   //  var num3=Number(window.prompt('Enter your Number')) ;
+   //   if(num1>num2 && num1>num2)
+   //   {
+   //      document.getElementById('demo').innerHTML =num1
+   //   }
+   //   else if(num2>num1 &&num2>num3)
+   //   {
+   //      document.getElementById('demo').innerHTML =num2
+   //   }
+
+   //   else if(num3>num1 &&num3>num2)
+   //   {
+   //      document.getElementById('demo').innerHTML =num3
+   //   }
+
+   
+
+    // Question:6
+    // var num1=Number(window.prompt('Enter your Number')) ;
+    // if(num1%2==0 )
+    // {
+
+    // document.getElementById('demo').innerHTML ='Even'
+
+    // }
+    // else
+    // {
+    //     document.getElementById('demo').innerHTML ='Odd'
+    // }
+
+    // Question:7
+    // var chars= window.prompt('Enter your chars');
+    // if(chars==='a' )
+    // {
+    //     document.getElementById('demo').innerHTML ='Vowel'
+    // }
+    // else if( chars=='e')
+    // {
+    //     document.getElementById('demo').innerHTML ='Vowel'
+    // }
+    // else if( chars=='l')
+    // {
+    //     document.getElementById('demo').innerHTML ='Vowel'
+    // }
+    // else if( chars=='o')
+    // {
+    //     document.getElementById('demo').innerHTML ='Vowel'
+    // }
+    // else if( chars=='u')
+    // {
+    //     document.getElementById('demo').innerHTML ='Vowel'
+    // }
+    // else
+    // {
+    //     document.getElementById('demo').innerHTML ='Consonant'
+    // }
+
+    // Question:8
+    // var limit=Number(window.prompt('Enter your Number')) ;
+    // for( var i=1; i<=limit;i++ )
+    // {
+    //    console.log(i)
+    // }
+    
+
+
+    //    Question:9
+    //    var limit=Number(window.prompt('Enter your Number')) ;
+    //     for( var i=1; i<=12;i++ )
+    //     {
+    //        console.log(i * limit)    
+    //     }
+
+
+
+    
+    // Question:10 
+    //     var num1=Number(window.prompt('Enter your Number')) ;
+    //     for( var i=1; i<=num1;i++ )
+    //     {
+    //         if(i % 2==0)
+    //         {
+    //             console.log(i)   
+    //         }
+            
+    //     }
+
+
+
+//     Question:11
+//     var num1=Number(window.prompt('Enter your Number')) ;
+//     var num2=Number(window.prompt('Enter your Number')) ;
+//       function power(num1 , num2)
+//   {
+//     var result1=num1;
+//     var result2=result1*result1 ;
+//     var result=result1*result2 ;
+//      document.getElementById('demo').innerHTML =result
+//   }
+//   power(num1,num2)
+
+// Question:12
+// var num1=Number(window.prompt('Enter your Number')) ;
+// var num2=Number(window.prompt('Enter your Number')) ;
+// var num3=Number(window.prompt('Enter your Number')) ;
+// var num4=Number(window.prompt('Enter your Number')) ;
+// var num5=Number(window.prompt('Enter your Number')) ;
+// function subject(a,b,c,d,e)
+// {
+//     var totalMarks=a+b+c+d+e;
+//     var averageMarks=totalMarks/5;
+//     var prestentage=averageMarks;
+
+//     console.log(totalMarks);
+//     console.log(averageMarks);
+//     console.log(prestentage);
+// }
+// subject(num1,num2,num3,num4,num5)
+
+
+// Question:13
+ 
+// var month=window.prompt('Enter your Month')
+
+//     if(month==1)
+//     {
+        
+//         document.getElementById('demo').innerHTML =31 +"days"
+//     }
+//     else if(month==2)
+//     {
+//         document.getElementById('demo').innerHTML =28 +"days"
+//     }
+//     else if(month==3)
+//     {
+//         document.getElementById('demo').innerHTML =31 +"days"
+//     }
+//     else if(month==4)
+//     {
+//         document.getElementById('demo').innerHTML =30 +"days"
+//     }
+//     else if(month==5)
+//     {
+//         document.getElementById('demo').innerHTML =31 +"days"
+//     }
+//     else if(month==6)
+//     {
+//         document.getElementById('demo').innerHTML =30 +"days"
+//     }
+//     else if(month==7)
+//     {
+//         document.getElementById('demo').innerHTML =31 +"days"
+//     }
+//     else if(month==8)
+//     {
+//         document.getElementById('demo').innerHTML =31 +"days"
+//     }
+//     else if(month==9)
+//     {
+//         document.getElementById('demo').innerHTML =30 +"days"
+//     }
+//     else if(month==10)
+//     {
+//         document.getElementById('demo').innerHTML =31 +"days"
+//     }
+//     else if(month==11)
+//     {
+//         document.getElementById('demo').innerHTML =30 +"days"
+//     }
+//     else if(month==12)
+//     {
+//          cdocument.getElementById('demo').innerHTML =31 +"days"
+//     }
+
+//     else
+//     {
+//         console.log('Not valid');
+//     }
+
+// Question:14
+
+// var sum,avg
+// var m1=Number(window.prompt('Physics'))
+// var m2= Number(window.prompt('Chemistry')) 
+// var m3=Number( window.prompt('Biology'))
+// var m4= Number(window.prompt('Mathematics'))
+// var m5=Number (window.prompt('Computer'))
+// sum=m1+m2+m3+m4+m5
+// console.log("total marks" ,sum)
+// avg=sum/5
+// console.log('average=',avg)
+
+// if(avg>=90){
+//   console.log("Grade A")
+// }
+// else if(avg>=80){
+//   console.log("Grade B")
+// }
+// else if(avg>=70){
+//   console.log("Grade C")
+// }
+// else if(avg>=60){
+//   console.log("Grade D")
+// }
+// else if(avg>=40){
+//   console.log("Grade E")
+// }
+// else if(avg<40){
+//   console.log("Grade F")
+// }
+// else{
+//   console.log("Fail")
+// }
+
+Question:15
+
+
+// var month=window.prompt('Enter your Month')
+// switch (month) {
+//     case '1':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     case '2':
+//         document.getElementById('demo').innerHTML =28 +"days"
+//         break;
+//     case '3':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     case '4':
+//         document.getElementById('demo').innerHTML =30 +"days"
+//         break;
+//     case '5':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     case '6':
+//         document.getElementById('demo').innerHTML =30 +"days"
+//         break;
+//     case '7':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     case '8':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     case '9':
+//         document.getElementById('demo').innerHTML =30 +"days"
+//         break;
+//     case '10':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     case '11':
+//         document.getElementById('demo').innerHTML =30 +"days"
+//         break;
+//     case '12':
+//         document.getElementById('demo').innerHTML =31 +"days"
+//         break;
+//     default:
+//         {
+//             document.getElementById('demo').innerHTML ='Enter Right Month'
+//         }
+//         break;
+// }
+
+
+Question:16
+
+// var chars= window.prompt('Enter your chars');
+// switch (chars) {
+//     case 'a':
+//         document.getElementById('demo').innerHTML ='Vowel'
+//         break;
+//     case 'e':
+//         document.getElementById('demo').innerHTML ='Vowel'
+//         break;
+//     case 'l':
+//         document.getElementById('demo').innerHTML ='Vowel'
+//         break;
+//     case 'o':
+//         document.getElementById('demo').innerHTML ='Vowel'
+//         break;
+//     case 'u':
+//         document.getElementById('demo').innerHTML ='Vowel'
+//         break;
+
+//     default:
+//         {
+//             document.getElementById('demo').innerHTML ='Consonant'
+//         }
+//         break;
+// }
+
+// Question:17
+
+// var num1=Number(window.prompt('Enter your Number')) ;
+// var num2=Number(window.prompt('Enter your Number')) ;
+
+// switch (num1 >num2) {
+//     case true :
+//         document.getElementById('demo').innerHTML =num1
+//         break;
+//     case false :
+//         document.getElementById('demo').innerHTML =num2
+//         break;
+//     default:
+//         {
+//             document.getElementById('demo').innerHTML ='Enter Valid Number '
+//         }
+//         break;
+// }
+
+
+
+//    Question:18
+
+//    var userNamber=Number(window.prompt('enter your number'));
+   
+//    switch (userNamber %2) {
+//     case  0 :
+//         document.getElementById('demo').innerHTML ='Even'
+//         break;
+//     case  1 :
+//         document.getElementById('demo').innerHTML ='odd'
+//         break;
+   
+//     default:
+//         {
+           
+//         }
+//         break;
+//    }
+   
+
+Question:19
+
+// var num1=Number(window.prompt('Enter your Number')) ;
+  
+
+// switch (num1 >0) {
+//     case true:
+//         document.getElementById('demo').innerHTML ='Postive'
+//         break;
+//     case false:
+//         document.getElementById('demo').innerHTML ='Negative'
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+Question:20
+
+// function clc()
+// {
+//     var numb1=Number(document.getElementById('numb1').value) ;
+//     var numb2=Number(document.getElementById('numb2').value) ;
+//     var op=document.getElementById('op').value ;
+
+//     switch (op) {
+//         case "*":
+//             var result=numb1*numb2;
+//             break;
+//         case "/" :
+//             var result=numb1/numb2;
+//             break;
+//         case "+" :
+//             var result=numb1+numb2;
+//             break;
+//         case "-" :
+//             var result=numb1-numb2;
+//             break;
+//         default:
+//             {
+//                 var result="operation not vailed"
+//             }
+//             break;
+//         }
+//     document.getElementById('demo1').innerHTML=result;
+   
+// }
